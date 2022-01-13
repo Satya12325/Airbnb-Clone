fb832a1e0405257e9229f0885cf9ff0bf6da86ae
Abhinav Sharma <abhinav.161a026@gmail.com>
2022-01-14 04:39:01 +0530

    hoteldetails

--- src/Router/AllpageRout.jsx
+++ src/Router/AllpageRout.jsx
@@ -1,6 +1,7 @@
 import {Route, Switch} from 'react-router-dom';
 import Home from '../Pages/Home';
 import HotelCityList from '../component/HotelCityList'
+import HotelDetail from '../Pages/HotelDetail';
 
 export default function AllpageRout(){
     return (
@@ -11,6 +12,9 @@ export default function AllpageRout(){
             <Route exact path="/hotels">
                     <HotelCityList/>
                 </Route>
+            <Route exact path='/hotels/:id'>
+                <HotelDetail/>
+            </Route>
         </Switch>
     )
 }
