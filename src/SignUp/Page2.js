3873bcc495dfc9f2b6d92a345e19d19c703be330
Sumant Singh <53209266+sumant236@users.noreply.github.com>
2022-01-15 00:22:06 +0530

    added media queries

--- src/SignUp/Page2.js
+++ src/SignUp/Page2.js
@@ -9,7 +9,7 @@ const styleso = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "70%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
