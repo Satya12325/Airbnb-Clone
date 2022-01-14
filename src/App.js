590194734d6051f29a601ec79c77b36081cf17eb
prajwal799 <prajwalgoyal1999@gmail.com>
2022-01-14 19:58:07 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import HotelCityList from './component/HotelCityList';
-import Home from "./Pages/Home.jsx";
-import Header from "./Header/Header.jsx"
-import Footer from "./Footer/Footer.jsx"
 
 import AllpageRout from './Router/AllpageRout';
 import BasicModal from './SignUp/Page2';
 function App() {
+  
+  
   return (
+
     <div className="App">
      {/* <BasicModal /> */}
       <AllpageRout/>
